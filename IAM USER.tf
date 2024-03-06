ff7176567f62df92ce5b136fe1ee8c7b77904181
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-03-06 18:38:29 +0530

    Create IAM USER.tf

---
+++ IAM USER.tf
@@ -0,0 +1,15 @@
+#basic create iam user and give permission
+
+
+provider "aws" {
+  region = "us-east-1"  # Change to your desired AWS region
+}
+
+resource "aws_iam_user" "karan" {
+  name = "karan"
+}
+
+resource "aws_iam_user_policy_attachment" "admin_access" {
+  user       = aws_iam_user.karan.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
