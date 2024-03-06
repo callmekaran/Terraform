0d255ef623dffbf2f2ee20979317d8663d676b15
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-03-06 18:54:21 +0530

    Update and rename IAM USER.tf to IAM User Creation.tf

--- IAM User Creation.tf
+++ IAM User Creation.tf
@@ -1,6 +1,5 @@
 #basic create iam user and give permission
 
-
 provider "aws" {
   region = "us-east-1"  # Change to your desired AWS region
 }
@@ -13,3 +12,13 @@ resource "aws_iam_user_policy_attachment" "admin_access" {
   user       = aws_iam_user.karan.id
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+resource "aws_iam_user_login_profile" "karan_login_profile" {
+  user                    = aws_iam_user.karan.name
+  password_reset_required = true  
+}
+
+output "karan_user_password" {
+  value = aws_iam_user_login_profile.karan_login_profile.password
+}
+
