debd44b19fe8899988151984f5a95e0c584470d1
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-03-07 17:34:43 +0530

    Update role.tf

--- IAM/role.tf
+++ IAM/role.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region     = "ap-south-1"
+}
+
 resource "aws_iam_role" "my_role" {
   name = "my_role"
   assume_role_policy = jsonencode({
@@ -22,4 +26,21 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "ssm-installation-role" 
   role = aws_iam_role.my_role.name 
 }
+        
+resource "aws_instance" "example_instance" {
+  ami           = "ami-03bb6d83c60fc5f7c"
+  instance_type = "t2.micro"
+  key_name      = "karan"
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt install amazon-ssm-agent amazon* -y
+              sudo systemctl start amazon-ssm-agent
+              EOF
+)
 
+  tags = {
+    Name = "example_instance"
+  }
+}
