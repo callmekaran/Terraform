f8f263c777b2b468970ae36a293305850f800a34
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-04-05 15:52:05 +0530

    Create output.tf

---
+++ SHELL_TERRAFORM/VPC/output.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = aws_vpc.discusit-vpc.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public-subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private-subnet.id
+}
