319f3a0595720a6d003a4342f2a4ae08a7d25fca
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-04-08 11:04:56 +0530

    Create variable.tf

---
+++ Lightsail/variable.tf
@@ -0,0 +1,8 @@
+variable "instance_name" {}
+variable "az" {}
+variable "os" {}
+variable "instance_type" {}
+variable "keypair" {}
+variable "region" {}
+variable "static_ip_name" {}
+variable "static_ip_attachment" {}
