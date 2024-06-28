f972e86ecf257b9d3a67b38c9b60e71b64fcfd26
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-06-28 19:19:42 +0530

    Update resource.tf

--- Modules/VPC_EC2/EC2/resource.tf
+++ Modules/VPC_EC2/EC2/resource.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ubuntu_server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
-  key_name               = "karan"
+  key_name               = var.key_name
   subnet_id              = var.public_subnet_id
   vpc_security_group_ids = [aws_security_group.test-server-sg.id]
   associate_public_ip_address = true
