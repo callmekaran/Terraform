92d04b93a64b948d9362e9f51506b626d9ee0213
Karan Ravat <karanravat60@gmail.com>
2024-07-09 13:18:18 +0530

    Initial Setup

--- RDS/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  access_key = "NA"
-  secret_key = "NADJG3eHkYm"
-  region     = "ap-south-1"
-}
--- VPC/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region  = "ap-south-1"
-  profile = "karan"
-}
