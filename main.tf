4c85d01061442df9a9a6f3f532405bdffc0cfa01
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-08-01 11:00:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,16 @@
 module "ssm-parameter" {
     source = "./modules/ssm"
 }
-
 module "sns-topic-creation" {
     source = "./modules/sns"
     email_addresses = var.email_addresses
     reference_sns_topic = module.sns-topic-creation.sns_topic_arn
 
 }
-
-
 module "staging-server-ec2" {
     source = "./modules/ec2"
     instance_id         = module.staging-server-ec2.instance_id
 }
-
 module "cloudwatch" {
   source = "./modules/cloudwatch"
   reference_sns_topic = module.sns-topic-creation.sns_topic_arn
