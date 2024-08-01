70e2c1c5e0659c49ee0eb5711b9d7eed7e17d4ff
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-08-01 12:45:00 +0530

    Update main.tf

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -1,3 +1,5 @@
+# CloudWatch alarm for CPU utilization
+
 resource "aws_cloudwatch_metric_alarm" "cpu_alarm" {
   alarm_name          = "Server Name - Ubuntu22Server, HighCPUUtilization"
   comparison_operator = "GreaterThanThreshold"
@@ -65,6 +67,54 @@ resource "aws_cloudwatch_metric_alarm" "disk_alarm" {
     path = "/"
     fstype = "ext4"
     device = "nvme0n1p1"
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
+
+# CloudWatch alarm for EC2 instance status check
+resource "aws_cloudwatch_metric_alarm" "instance_status_check_alarm" {
+  alarm_name          = "InstanceStatusCheckFailure-${var.instance_id}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "StatusCheckFailed_Instance"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Maximum"
+  threshold           = 0
+  alarm_description   = "This metric monitors the instance status check failures"
+  actions_enabled     = true
+  alarm_actions       = [var.reference_sns_topic]
+  ok_actions          = [var.reference_sns_topic]
+
+  dimensions = {
+    InstanceId = var.instance_id
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
+
+# CloudWatch alarm for system status check
+resource "aws_cloudwatch_metric_alarm" "system_status_check_alarm" {
+  alarm_name          = "SystemStatusCheckFailure-${var.instance_id}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "StatusCheckFailed_System"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Maximum"
+  threshold           = 0
+  alarm_description   = "This metric monitors the system status check failures"
+  actions_enabled     = true
+  alarm_actions       = [var.reference_sns_topic]
+  ok_actions          = [var.reference_sns_topic]
+
+  dimensions = {
+    InstanceId = var.instance_id
   }
 
   tags = {
