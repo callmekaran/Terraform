8d5884b9c4a3bcf43fb25d35d07d49a97e89d267
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-08-01 10:57:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,23 @@
-module "vpc" {
-  source                    = "./modules/vpc"
-  public_subnet_cidr_block  = var.public_subnet_cidr_block
-  vpc_tags                  = var.vpc_tags
-  public_subnet_tags        = var.public_subnet_tags
-  igw_tags                  = var.igw_tags
-  route_cidr_block          = var.route_cidr_block
-  public_route_tags         = var.public_route_tags
-  vpc_cidr_block            = var.vpc_cidr_block
-  private_route_tags        = var.private_route_tags
-  private_subnet_cidr_block = var.private_subnet_cidr_block
-  private_subnet_tags       = var.private_subnet_tags
+module "ssm-parameter" {
+    source = "./modules/ssm"
+}
+
+module "sns-topic-creation" {
+    source = "./modules/sns"
+    email_addresses = var.email_addresses
+    reference_sns_topic = module.sns-topic-creation.sns_topic_arn
 
 }
 
-module "ec2" {
-  source           = "./modules/ec2"
-  vpc_id           = module.vpc.vpc_id
-  public_subnet_id = module.vpc.public_subnet_id
+
+module "staging-server-ec2" {
+    source = "./modules/ec2"
+    instance_id         = module.staging-server-ec2.instance_id
+}
+
+module "cloudwatch" {
+  source = "./modules/cloudwatch"
+  reference_sns_topic = module.sns-topic-creation.sns_topic_arn
+  instance_id         = module.staging-server-ec2.instance_id
+  depends_on = [ module.staging-server-ec2 ]
 }
