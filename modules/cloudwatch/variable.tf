9052e3ba9c751ff5d7403e97f5e999b3db1aac11
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-08-01 11:06:46 +0530

    Create variable.tf

---
+++ modules/cloudwatch/variable.tf
@@ -0,0 +1,8 @@
+variable "reference_sns_topic" {
+  description = "SNS topic ARN for alerts"
+  type        = string
+}
+variable "instance_id" {
+  description = "EC2 InstanceID"
+  type        = string  
+}
