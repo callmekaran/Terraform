87661dabb75fd52d846350f367416118050fd19c
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-08-01 11:02:50 +0530

    Update variable.tf

--- modules/ec2/variable.tf
+++ modules/ec2/variable.tf
@@ -1,11 +1,7 @@
-variable "public_subnet_id" {
-  type = string
-}
-
-variable "vpc_id" {
-  type = string
-}
-
 variable "key_name" {
   default = "ravat" # Default value, can be overridden
+}
+variable "instance_id" {
+  description = "EC2 InstanceID"
+  type        = string  
 }
