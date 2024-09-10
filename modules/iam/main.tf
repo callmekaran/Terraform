32a159b8b3556ced3635523972865728a3b671c4
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-09-10 13:24:28 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -14,3 +14,39 @@ resource "aws_iam_policy" "example" {
     ]
   })
 }
+
+resource "aws_iam_role" "ssm_role" {
+  name = "ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ssm.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "ssm_role_policy_attachment" {
+  name       = "ssm-role-policy-attachment"
+  roles      = [aws_iam_role.ssm_role.name]
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_policy_attachment" "ssm_role_policy_attachment_session" {
+  name       = "ssm-role-policy-attachment-session"
+  roles      = [aws_iam_role.ssm_role.name]
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+}
+
+resource "aws_iam_policy_attachment" "secret-manager-fetch-secret-attachment-policy" {
+  name       = "secret-manager-fetch-secret-attachment-policy"
+  roles      = [aws_iam_role.ssm_role.name]
+  policy_arn  = aws_iam_policy.example.arn
+}
+
