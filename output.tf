4d57674c8ab5b3fdd80c6be13891aebd21a148d9
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-09-11 20:15:15 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,19 @@
-output "secret_arn" {
-  value = module.secret_manager.secret_arn
+output "vpc2-id" {
+    value = module.vpc2.vpc2-id
+}
+
+output "vpc1-id" {
+  value = module.vpc1.vpc1-id
+}
+
+output "vpc1-route-table-id" {
+  value = module.vpc1.vpc1-route-table-id
+}
+
+output "vpc2-route-table-id" {
+  value = module.vpc2.vpc2-route-table-id
+}
+
+output "vpc-peering-connection-id" {
+  value = module.vpc-peerings.vpc-peering-connection-id
 }
