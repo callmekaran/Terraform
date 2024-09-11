eb2a1ae98f410f8ab4f2ecf4760f621c4a8b1f81
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-09-11 20:14:46 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,2 +1,5 @@
-variable "reference_secret_manager" {
-}
+variable "refrence-vpc2-id" {}
+variable "refrence-vpc1-id" {}
+variable "refrence-vpc1-rt-id" {}
+variable "refrence-vpc2-rt-id" {}
+variable "refrence-vpc-peering-connection-id" {}
