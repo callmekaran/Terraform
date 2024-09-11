9ff43f38a4aa2b1eb1d71971899148952ccf5e61
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-09-11 20:19:06 +0530

    Create variable.tf

---
+++ modules/vpc-peering/variable.tf
@@ -0,0 +1,14 @@
+variable "refrence-vpc1-id" {
+  type = string
+}
+
+variable "refrence-vpc2-id" {
+  type = string
+}
+
+variable "refrence-vpc-peering-connection-id" {
+    type = string
+}
+
+variable "refrence-vpc1-rt-id" {}
+variable "refrence-vpc2-rt-id" {}
