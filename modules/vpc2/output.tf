7ea948fe2fa2335344b1e96b13be9b1942d1b554
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-09-12 16:30:31 +0530

    Update output.tf

--- modules/vpc2/output.tf
+++ modules/vpc2/output.tf
@@ -1,7 +1,11 @@
 output "vpc2-id" {
-    value = aws_vpc.vpc2.id
+  value = aws_vpc.vpc2.id
 }
 
 output "vpc2-route-table-id" {
   value = aws_route_table.vpc2-public-route-table.id
+}
+
+output "vpc2-public-subnet-cidr-block" {
+  value = aws_subnet.vpc2-public-subnet.cidr_block
 }
