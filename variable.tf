d6d78b345233f1e27239450ced219a3ea17c7759
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-09-12 16:29:01 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,3 +3,5 @@ variable "refrence-vpc1-id" {}
 variable "refrence-vpc1-rt-id" {}
 variable "refrence-vpc2-rt-id" {}
 variable "refrence-vpc-peering-connection-id" {}
+variable "refrence-vpc2-public-subnet-cidr-block" {}
+variable "refrence-vpc1-public-subnet-cidr-block" {}
