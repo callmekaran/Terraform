ed66bcd067c5f9bca39b34ee2d6393347da82466
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-09-12 16:32:18 +0530

    Update main.tf

--- modules/vpc-peering/main.tf
+++ modules/vpc-peering/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "vpc-1to-vpc2-peering-connection" {
-  vpc_id        = var.refrence-vpc1-id
-  peer_vpc_id   = var.refrence-vpc2-id
-  auto_accept   = true
+  vpc_id      = var.refrence-vpc1-id
+  peer_vpc_id = var.refrence-vpc2-id
+  auto_accept = true
 
   tags = {
     Name = "vpc-1to-vpc2-peering-connection"
@@ -18,13 +18,13 @@ resource "aws_vpc_peering_connection" "vpc-1to-vpc2-peering-connection" {
 }
 
 resource "aws_route" "vpc1-to-vpc2-peering-route" {
-  route_table_id         = var.refrence-vpc1-rt-id
-  destination_cidr_block = "172.158.1.0/24"  # CIDR block of VPC2
+  route_table_id            = var.refrence-vpc1-rt-id
+  destination_cidr_block    = var.refrence-vpc2-public-subnet-cidr-block # CIDR block of VPC1
   vpc_peering_connection_id = var.refrence-vpc-peering-connection-id
 }
 
 resource "aws_route" "vpc2-to-vpc1-peering-route" {
-  route_table_id         = var.refrence-vpc2-rt-id
-  destination_cidr_block = "192.168.1.0/24"  # CIDR block of VPC2
+  route_table_id            = var.refrence-vpc2-rt-id
+  destination_cidr_block    = var.refrence-vpc1-public-subnet-cidr-block # CIDR block of VPC2
   vpc_peering_connection_id = var.refrence-vpc-peering-connection-id
 }
