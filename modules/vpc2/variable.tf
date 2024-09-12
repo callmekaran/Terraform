9011ab8d0a39d235d2455997bf1ab53d9f752924
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-09-12 16:30:17 +0530

    Update variable.tf

--- modules/vpc2/variable.tf
+++ modules/vpc2/variable.tf
@@ -1,2 +1,15 @@
 variable "refrence-vpc2-id" {}
 variable "refrence-vpc2-rt-id" {}
+variable "refrence-vpc2-public-subnet-cidr-block" {}
+variable "vpc2-private-subnet-cidr-block" {
+  default = "172.158.2.0/24"
+}
+
+variable "vpc2-public-subnet-cidr-block" {
+  default = "172.158.1.0/24"
+}
+
+variable "vpc2-cidr-block" {
+  default = "172.158.0.0/16"
+}
+
