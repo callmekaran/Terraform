d4b06c29f62b216efbdb23a004b062939b30d671
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-09-12 16:31:34 +0530

    Update main.tf

--- modules/vpc1/main.tf
+++ modules/vpc1/main.tf
@@ -1,31 +1,30 @@
 resource "aws_vpc" "vpc1" {
-    cidr_block = "192.168.0.0/16"
-    enable_dns_support = true    # Enables DNS support for the VPC
-    enable_dns_hostnames = true  # Enables DNS hostnames for the VPC
+  cidr_block           = var.vpc1-cidr-block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-    
-
-    tags = {
+  tags = {
     Name = "vpc1"
   }
 }
 
+
 resource "aws_subnet" "vpc1-public-subet" {
-    cidr_block = "192.168.1.0/24"
-    vpc_id = aws_vpc.vpc1.id
-    tags = {
-      Name = "vpc2-public-subnet"
-    }
-  
+  cidr_block = var.vpc1-public-subnet-cidr-block
+  vpc_id     = aws_vpc.vpc1.id
+  tags = {
+    Name = "vpc1-public-subnet"
+  }
+
 }
 
 resource "aws_subnet" "vpc1-private-subet" {
-    cidr_block = "192.168.2.0/24"
-    vpc_id = aws_vpc.vpc1.id
-    tags = {
-      Name = "vpc2-private-subnet"
-    }
-  
+  cidr_block = var.vpc1-private-subnet-cidr-block
+  vpc_id     = aws_vpc.vpc1.id
+  tags = {
+    Name = "vpc1-private-subnet"
+  }
+
 }
 
 resource "aws_internet_gateway" "vpc1-internet_gateway" {
