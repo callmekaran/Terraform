5fc57430d8f46156ad3679d8a83fa5db2dbb1693
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-09-12 16:32:30 +0530

    Update output.tf

--- modules/vpc-peering/output.tf
+++ modules/vpc-peering/output.tf
@@ -1,3 +1,3 @@
 output "vpc-peering-connection-id" {
-    value = aws_vpc_peering_connection.vpc-1to-vpc2-peering-connection.id
+  value = aws_vpc_peering_connection.vpc-1to-vpc2-peering-connection.id
 }
