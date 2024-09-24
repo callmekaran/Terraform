5aeb94a440ef8366a4f833a481a36dfe5a996acf
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-09-24 14:10:10 +0530

    Create main.tf

---
+++ Another_Perceptions/main.tf
@@ -0,0 +1,19 @@
+module "ssm-parameter" {
+    source = "./modules/ssm"
+}
+module "sns-topic-creation" {
+    source = "./modules/sns"
+    email_addresses = var.email_addresses
+    reference_sns_topic = module.sns-topic-creation.sns_topic_arn
+
+}
+module "staging-server-ec2" {
+    source = "./modules/ec2"
+    instance_id         = var.instance_id
+}
+module "cloudwatch" {
+  source = "./modules/cloudwatch"
+  reference_sns_topic = module.sns-topic-creation.sns_topic_arn
+  instance_id         = var.instance_id
+  depends_on = [ module.staging-server-ec2 ]
+}
