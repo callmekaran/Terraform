c603916ac8c4bc94fb7240f9ccc7c3828ab4f94b
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-10-10 18:19:25 +0530

    Update main.tf

--- DO/project-vpc-droplet/main.tf
+++ DO/project-vpc-droplet/main.tf
@@ -1,39 +1,63 @@
 resource "digitalocean_vpc" "example" {
-  name     = "example-project-network"
-  region   = "nyc3"
-  ip_range = "10.10.10.0/24"
+  name     = var.vpc-name
+  region   = var.region
+  ip_range = var.ip_range
 }
 
-
 resource "digitalocean_project" "rlogical-devops" {
-  name        = "Rlogical-Devops"
+  name        = var.project-name
   description = "A project to represent development resources."
   purpose     = "Web Application"
-  environment = "Development"
+  environment = var.environment
 }
 
 resource "digitalocean_droplet" "foobar" {
-  name   = "example"
-  size   = "s-1vcpu-1gb"
-  image  = "ubuntu-22-04-x64"
-  region = "nyc3"
+  name   = var.droplet_tag
+  size   = var.instance_type
+  image  = var.ami
+  region = var.region
   monitoring = true
   backups = true
   vpc_uuid = digitalocean_vpc.example.id
   ssh_keys = [digitalocean_ssh_key.default.id]  
-  tags = [ "Devops-Test" ]
+  tags = [var.droplet_tag]
 }
+
 
 resource "digitalocean_project_resources" "barfoo" {
   project = digitalocean_project.rlogical-devops.id
   resources = [
     digitalocean_droplet.foobar.urn,
     digitalocean_vpc.example.urn,
-    digitalocean_ssh_key.default.id 
+    digitalocean_ssh_key.default.id # Using .urn for the resource URN
   ]
 }
 
 resource "digitalocean_ssh_key" "default" {
   name       = "Terraform Example"
   public_key = file("/home/rlogical-lap-23/DO/test.pub")
+}
+
+locals {
+  alert_types = {
+    "v1/insights/droplet/cpu"                    = "CPU"
+    "v1/insights/droplet/disk_utilization_percent" = "Disk"
+    "v1/insights/droplet/memory_utilization_percent" = "Memory"
+  }
+}
+
+resource "digitalocean_monitor_alert" "alerts" {
+  for_each = local.alert_types
+
+  alerts {
+    email = ["karan.ravat@digitalocean.com"]
+  }
+  
+  window      = "5m"
+  type        = each.key
+  compare     = "GreaterThan"
+  value       = 75
+  enabled     = true
+  entities    = [digitalocean_droplet.foobar.id]
+  description = "Alert for ${each.value} usage above 75% for Droplet Tag: ${var.droplet_tag}"
 }
