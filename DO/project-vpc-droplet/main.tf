16887d2ae9646643065ec712ff3aad95d432796b
callmekaran <69335113+callmekaran@users.noreply.github.com>
2024-10-10 18:27:54 +0530

    Update main.tf

--- DO/project-vpc-droplet/main.tf
+++ DO/project-vpc-droplet/main.tf
@@ -61,3 +61,39 @@ resource "digitalocean_monitor_alert" "alerts" {
   entities    = [digitalocean_droplet.foobar.id]
   description = "Alert for ${each.value} usage above 75% for Droplet Tag: ${var.droplet_tag}"
 }
+
+
+resource "digitalocean_firewall" "web-firewall" {
+  name = "only-22-80-and-443"
+
+  droplet_ids = [digitalocean_droplet.foobar.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["192.168.1.0/24"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
