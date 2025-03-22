ba9aee1fd3f08e5c38c239450b67488d903e4ce7
callmekaran <69335113+callmekaran@users.noreply.github.com>
2025-03-22 15:49:39 +0530

    Update data.tf

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-*-24.04-amd64-server-*"]
   }
 
   filter {
@@ -11,5 +11,5 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"]
+  owners = [ "amazon" ]
 }
